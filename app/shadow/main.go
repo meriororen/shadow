deb84a84e3a145700fa7349f77053d7ca0dea3fa
Isa Ansharullah <isa@dekape.co.id>
2019-10-25 22:01:58 +0700

    fix watcher

--- app/shadow/main.go
+++ app/shadow/main.go
@@ -1,37 +1,26 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	/*
-		"os"
-		"time"
-
-		"shadow/mqtt"
-	*/
+	"log"
 
 	"shadow/watcher"
-
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
 
 	pmg "github.com/eclipse/paho.mqtt.golang"
 	"github.com/subosito/gotenv"
 )
 
 var (
-	mq              *pmg.Client
-	watchConfigList []string
-	watchList       []Actor
+	mq *pmg.Client
 )
 
 func main() {
 	gotenv.Load()
 
-	cli, err := client.NewEnvClient()
-	if err != nil {
-		fmt.Println("Cannot initialize client: ", err)
-	}
+	wt := watcher.NewWatcher()
+	wt.AddImageToWatch("golang:1.13-alpine")
+	wt.AddImageToWatch("registry.gitlab.com/sangkuriang-dev/transmissor-be/backend:devel")
+
+	//	wt.WatchAll()
 
 	/*
 		mq = &mqtt.NewClient(
@@ -43,38 +32,7 @@ func main() {
 		)
 	*/
 
-	watchConfigList = []string{"registry.gitlab.com/sangkuriang-dev/transmissor-be/backend:devel", "golang/1.13-alpine"}
-
-	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
-	if err != nil {
-		fmt.Println("Cannot get container list : ", err)
+	for _, w := range wt.WatchList {
+		log.Println((*w).ImageNames, " => ", (*w).ContainerIDs)
 	}
-
-	images, err := cli.ImageList(context.Background(), types.ImageListOptions{})
-
-	for _, image := range images {
-		for _, t := range image.RepoTags {
-			for _, w := range watchConfigList {
-				if w == t {
-					//fmt.Printf("%v %v is being watched\n", image.Containers, image.RepoTags)
-					watchList = append(watchList, Actor{ImageNames: image.RepoTags})
-				}
-			}
-		}
-	}
-
-	for i, w := range watchList { // among the watched list
-		for _, in := range w.ImageNames { // which has these images alias
-			for _, container := range containers { // find containers
-				fmt.Printf("<%v> (%v)", container.ID, container.Image)
-				if container.Image == in {
-					//fmt.Printf("container id %s, with image %s is being watched", container.ID[:10], in)
-					watchList[i].ContainerIDs = append(watchList[i].ContainerIDs, container.ID)
-				}
-				fmt.Printf("\n")
-			}
-		}
-	}
-
-	fmt.Println(watchList)
 }
--- watcher/watcher.go
+++ watcher/watcher.go
@@ -1,12 +1,20 @@
 package watcher
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"time"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 )
 
 type Watcher struct {
-	WatchList       []Actor
+	WatchList       map[string]*Actor
 	WatchConfigList []string
+	cli             *client.Client
+	heartBeatPeriod time.Duration
 }
 
 type Actor struct {
@@ -20,17 +28,47 @@ type Container interface {
 	Remove(container string)
 }
 
+type containerRunConfig struct {
+}
+
 type Image interface {
 	Run(image string, conf containerRunConfig)
 }
 
-func NewWatcher(period time.Duration) *Watcher {
+func NewWatcher() *Watcher {
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		fmt.Println("Cannot initialize client: ", err)
+	}
+
+	return &Watcher{
+		WatchList:       make(map[string]*Actor),
+		WatchConfigList: []string{},
+		cli:             cli,
+	}
 }
 
-func (w *Watcher) Watch() {
-	go func() {
+func (w *Watcher) WatchImages() {
+	for _, actor := range w.WatchList {
+		for image := range actor.ImageNames {
+			image = image
+		}
+	}
+}
 
-	}()
+func (w *Watcher) WatchContainer() {
+	for _, actor := range w.WatchList {
+		for cont := range actor.ContainerIDs {
+			cont = cont
+		}
+	}
+}
+
+func (w *Watcher) WatchAll() {
+	for {
+		w.WatchImages()
+		w.WatchContainer()
+	}
 }
 
 func (w *Watcher) isInWatchConfigList(imageName string) int {
@@ -43,16 +81,55 @@ func (w *Watcher) isInWatchConfigList(imageName string) int {
 }
 
 func (w *Watcher) AddImageToWatch(imageName string) {
-	if isInWatchConfigList(imageName) == -1 {
+	if w.isInWatchConfigList(imageName) == -1 {
 		w.WatchConfigList = append(w.WatchConfigList, imageName)
 	}
+
+	w.addImagesToWatchList()
+	w.addRunningContainersToWatchList()
 }
 
 func (w *Watcher) RemoveImageFromWatchList(imageName string) {
-	if index := isInWatchConfigList(imageName); index != -1 {
+	if index := w.isInWatchConfigList(imageName); index != -1 {
 		l := len(w.WatchConfigList)
 		w.WatchConfigList[index] = w.WatchConfigList[l-1]
 		w.WatchConfigList[l-1] = ""
 		w.WatchConfigList = w.WatchConfigList[:l-1]
+		// remove from watchlist
+		delete(w.WatchList, imageName)
+	}
+}
+
+func (w *Watcher) addImagesToWatchList() {
+	images, err := w.cli.ImageList(context.Background(), types.ImageListOptions{})
+	if err != nil {
+		log.Fatal("Cannot add image in local to watchlist ", err)
+	}
+
+	for _, image := range images {
+		for _, t := range image.RepoTags {
+			for _, watch := range w.WatchConfigList {
+				if watch == t {
+					w.WatchList[watch] = &Actor{ImageNames: image.RepoTags}
+				}
+			}
+		}
+	}
+}
+
+func (w *Watcher) addRunningContainersToWatchList() {
+	containers, err := w.cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		log.Fatal("Cannot get container list : ", err)
+	}
+
+	for _, actor := range w.WatchList {
+		for _, in := range actor.ImageNames {
+			for _, container := range containers {
+				if container.Image == in {
+					(*actor).ContainerIDs = append(actor.ContainerIDs, container.ID)
+				}
+			}
+		}
 	}
 }
